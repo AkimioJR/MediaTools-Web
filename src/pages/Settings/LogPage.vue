04b9ed12a030894ee0c131f8d7949652d9bbc821
Akimio521 <akimio2333@gmail.com>
2025-08-07 18:33:09 +0800

    feat(LogPage): 添加日志配置页面以支持日志级别和路径设置

---
+++ src/pages/Settings/LogPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <template v-if="logConfig">
+    <v-select
+      v-model="logConfig.level"
+      label="日志级别"
+      :items="logLevels"
+      item-title="label"
+      item-value="value"
+      class="mb-4"
+      clearable
+    />
+    <v-text-field
+      v-model="logConfig.path"
+      label="日志路径"
+      class="mb-4"
+      clearable
+    />
+  </template>
+  <v-btn color="primary" class="mt-4" @click="updateData"> 更新日志配置 </v-btn>
+</template>
+
+<script lang="ts" setup name="LogPage">
+import { defineExpose } from "vue";
+import { useLogConfig } from "@/hooks";
+
+const { logConfig, loadData, updateData } = useLogConfig();
+
+// 定义日志级别选项
+const logLevels = [
+  { label: "Debug", value: "debug" },
+  { label: "Info", value: "info" },
+  { label: "Warning", value: "warning" },
+  { label: "Error", value: "error" },
+];
+
+defineExpose({ loadData });
+</script>
+
+<style scoped></style>
