3b85be70af9c585021826e1d5e773546e0ef3d8b
Akimio521 <akimio2333@gmail.com>
2025-08-11 11:30:09 +0800

    feat: 跟随上游 API 变化

--- src/services/log.ts
+++ src/services/log.ts
@@ -1,7 +1,8 @@
 import api from "@/services/api";
+import type { LogDetail } from "@/types";
 
 export const LogService = {
-  async GetRecentLogs(): Promise<string[]> {
+  async GetRecentLogs(): Promise<LogDetail[]> {
     return await api.get("/log/recent");
   },
 };
