5777824e5fc1ad7fb0d857e1871fb7598dbc3a6a
Akimio521 <akimio2333@gmail.com>
2025-08-11 18:49:42 +0800

    feat: 添加 GlobalMessage 组件以支持全局消息通知

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
   <v-app id="inspire">
     <AppNavigation />
     <AppHeader />
-    <v-main> <router-view /> </v-main>
+    <v-main>
+      <GlobalMessage />
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
 import AppNavigation from "@/layouts/AppNavigation.vue";
 import AppHeader from "@/layouts/AppHeader.vue";
+import GlobalMessage from "@/components/GlobalMessage.vue";
 </script>
---
+++ src/components/GlobalMessage.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-snackbar
+    v-model="globalMessageState.show"
+    :color="globalMessageState.color"
+    :timeout="globalMessageState.timeout"
+    location="top"
+  >
+    <template v-slot:actions>
+      <v-btn
+        variant="text"
+        @click="hideMessage"
+        icon="mdi-close"
+        size="small"
+      />
+    </template>
+    {{ globalMessageState.message }}
+  </v-snackbar>
+</template>
+
+<script setup lang="ts">
+import { globalMessageState, hideMessage } from "@/utils/message";
+</script>
