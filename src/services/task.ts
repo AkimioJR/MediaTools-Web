6c0d4949020610be8be41019d4e4431fa3102b15
Akimio521 <akimio2333@gmail.com>
2025-08-25 11:52:11 +0800

    feat: 添加任务服务模块及相关方法

---
+++ src/services/task.ts
@@ -0,0 +1,16 @@
+import type { Task } from '@/types'
+import api from './api'
+
+export const TaskService = {
+  TransferTaskService: {
+    async GetAllTasks(): Promise<Task[]> {
+      return await api.get('/task/transfer')
+    },
+    async GetTask(task_id: string): Promise<Task> {
+      return await api.get(`/task/transfer/${task_id}`)
+    },
+    async CancelTask(task_id: string): Promise<Task> {
+      return await api.delete(`/task/transfer/${task_id}`)
+    },
+  },
+}
