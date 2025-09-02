c16e41a7ffff184345cca288b2eca9f2127a0965
Akimio521 <akimio2333@gmail.com>
2025-09-02 19:04:10 +0800

    update: 跟随后端更新 API

--- src/services/history.ts
+++ src/services/history.ts
@@ -3,7 +3,6 @@ import type { StorageType, TransferType, MediaTransferHistory } from '@/types'
 import api from './api'
 
 export interface MediaHistoryQueryOptions {
-  id?: number // 数据库主键ID,若传入则至多返回一条记录
   startTime?: string // 开始时间，RFC3339 格式
   endTime?: string // 结束时间，RFC3339 格式
   storageType?: StorageType // 存储类型
@@ -20,7 +19,6 @@ export const HistoryService = {
       options: MediaHistoryQueryOptions = {},
     ): Promise<MediaTransferHistory[]> {
       const {
-        id,
         startTime,
         endTime,
         storageType,
@@ -35,26 +33,20 @@ export const HistoryService = {
 
       let params: Record<string, unknown> = {}
 
-      if (isDefined(id)) {
-        params = { id }
-      } else {
-        const candidateParams: Record<string, unknown> = {
-          start_time: startTime,
-          end_time: endTime,
-          storage_type: storageType,
-          path,
-          transfer_type: transferType,
-          status,
-          count,
-          page,
-        }
-
-        params = Object.fromEntries(
-          Object.entries(candidateParams).filter(([, value]) =>
-            isDefined(value),
-          ),
-        )
+      const candidateParams: Record<string, unknown> = {
+        start_time: startTime,
+        end_time: endTime,
+        storage_type: storageType,
+        path,
+        transfer_type: transferType,
+        status,
+        count,
+        page,
       }
+
+      params = Object.fromEntries(
+        Object.entries(candidateParams).filter(([, value]) => isDefined(value)),
+      )
 
       return await api.get('/history/media', { params })
     },
