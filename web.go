7e31d2cf533ebdf4999b396caf8503ab449bbd50
Akimio521 <akimio2333@gmail.com>
2025-09-28 18:28:21 +0800

    feat: 从 WebDist 读取图标，减少嵌入文件大小

--- web.go
+++ web.go
@@ -2,10 +2,21 @@ package web
 
 import (
 	"embed"
+	"io"
 )
 
 //go:embed dist
 var WebDist embed.FS
 
-//go:embed dist/favicon.ico
-var IconData []byte
+func GetIconData() []byte {
+	icon, err := WebDist.Open("dist/favicon.ico")
+	if err != nil {
+		panic("缺少图标资源: " + err.Error())
+	}
+	defer icon.Close()
+	iconData, err := io.ReadAll(icon)
+	if err != nil {
+		panic("读取图标资源失败: " + err.Error())
+	}
+	return iconData
+}
