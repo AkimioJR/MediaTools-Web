10b62753a2860d11b189f4b880e6a8425cea82ec
Akimio521 <akimio2333@gmail.com>
2025-09-28 18:30:56 +0800

    feat: 提取读取逻辑

--- web.go
+++ web.go
@@ -2,21 +2,26 @@ package web
 
 import (
 	"embed"
+	"fmt"
 	"io"
 )
 
 //go:embed dist
 var WebDist embed.FS
 
+func getFileData(path string) []byte {
+	f, err := WebDist.Open(path)
+	if err != nil {
+		panic(fmt.Sprintf("打开 %s 资源失败: %v", path, err))
+	}
+	defer f.Close()
+	data, err := io.ReadAll(f)
+	if err != nil {
+		panic(fmt.Sprintf("读取 %s 资源失败: %v", path, err))
+	}
+	return data
+}
+
 func GetIconData() []byte {
-	icon, err := WebDist.Open("dist/favicon.ico")
-	if err != nil {
-		panic("缺少图标资源: " + err.Error())
-	}
-	defer icon.Close()
-	iconData, err := io.ReadAll(icon)
-	if err != nil {
-		panic("读取图标资源失败: " + err.Error())
-	}
-	return iconData
+	return getFileData("dist/favicon.ico")
 }
