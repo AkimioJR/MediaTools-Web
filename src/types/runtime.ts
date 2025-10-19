0d1e0689f62e1b4a9242c2d4cd6c02dba81ccaf4
AkimioJR <akimio.jr@gmail.com>
2025-10-19 22:38:15 +0800

    feat: 添加操作系统和架构字段到 RuntimeAppStatusInfo 接口

--- src/types/runtime.ts
+++ src/types/runtime.ts
@@ -3,4 +3,6 @@ export interface RuntimeAppStatusInfo {
     is_dev: boolean // 当前是否为开发模式
     port: number // 当前运行端口（桌面模式下为 0）
     boot_time: string // 程序启动时间，RFC3339 字符串
+    os: string // 操作系统
+    arch: string // 架构
 }
